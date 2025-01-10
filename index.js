c325b7a3202330451cb799b8a0f974f9b4c8f125
Pradeep kumar singh <pradeepksin999@gmail.com>
2025-01-10 21:26:11 +0530

    html file

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-console.log("Hello world") 
 
-console.log("Hi...")
+// // let rearrangeFruits= splice.fruits;
+
+// console.log(fruits);
+
+
+function rearrangeFruits(){
+
+    
+    console.log(rearrangeFruits(fruits));
+    }
+
+    let fruits = ["Apple", "Banana", "Orange", "Mango", "Pineapple", "Grapes", "Strawberry", "Watermelon", "Peach", "Kiwi"];
+
